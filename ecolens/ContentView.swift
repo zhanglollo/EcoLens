aa2ecc93eb384a13113ec4d9ce1a3f847a753714
zhanglollo <zhanglitong09@gmail.com>
2025-04-07 18:17:18 -0400

    Initial Commit

---
+++ ecolens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ecolens
+//
+//  Created by Litong Zhang on 2025-04-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ecolensTests/ecolensTests.swift
@@ -0,0 +1,17 @@
+//
+//  ecolensTests.swift
+//  ecolensTests
+//
+//  Created by Litong Zhang on 2025-04-07.
+//
+
+import Testing
+@testable import ecolens
+
+struct ecolensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
